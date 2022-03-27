54d3582505b921327380b835ff3bff5ccd427621
developertobi <developertobi@gmail.com>
2022-03-27 12:34:55 +0100

    Refactored the codebase a bit, added some necessary firbase dependencies, implement firebase registration with email and password.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
 import '../widgets/rounded_button.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -8,6 +10,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  late String email;
+  late String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,23 +37,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration:
+                  kAuthInputDecoration.copyWith(hintText: 'Enter your email.'),
             ),
             SizedBox(
               height: 8.0,
@@ -55,22 +48,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
+              decoration: kAuthInputDecoration.copyWith(
+                hintText: 'Enter your Password.',
               ),
             ),
             SizedBox(
@@ -78,9 +59,21 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             RoundedButton(
               buttonText: 'Register',
-              onPressed: () {
+              onPressed: () async {
                 //Go to login screen.
-                Navigator.pushNamed(context, '/registration');
+                print(email);
+                print(password);
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, '/chat');
+                  }
+                } catch (e) {
+                  print('Error $e');
+                }
               },
               color: Colors.blueAccent,
             ),
--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -24,7 +24,10 @@ class RoundedButton extends StatelessWidget {
           onPressed: onPressed,
           minWidth: 200.0,
           height: 42.0,
-          child: Text(buttonText),
+          child: Text(
+            buttonText,
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
